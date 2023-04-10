a9417ecfc60effe81e16ebb5fe0bfe0276dbd2d5
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-04-10 10:43:27 +0800

    push

--- Day 1 Exercise/car_list.py
+++ Day 1 Exercise/car_list.py
@@ -18,15 +18,34 @@ class Car:
     def __str__(self):
         return(self.__model+" "+self.__registration_number+" "+(str)(self.__year))
     
-    #Implement Service class here
-    def find_cars_by_year (year):
-        pass
+    # Write Service Class as given in class diagram that performs various activities on a list of cars
+    # Implement Service class here
+
+class Service:
+    def __init__(self, car_list):
+        self.__car_list = car_list
+        
+    def get_car_list(self):
+        self.__car_list    
+        
+    def find_cars_by_year (self, year):
+        list1 = []
+        for car in self.__car_list:
+            if int(car.get_year()) == year:
+                list1.append(car.get_model())
+        if len(list1) == 0:
+            return None
+        return list1
     
-    def add_cars (new_car_list):
-        pass
+    def add_cars (self, new_car_list):
+        self.__car_list.extend(new_car_list)
+        self.__car_list.sort(key = lambda x: x.get_year())
     
-    def remove_cars_from_karnataka ():
-        pass
+    def remove_cars_from_karnataka (self):
+        list1 = self.__car_list.copy()
+        for car in list1:
+            if car.get_registration_number()[0:2] == "KA":
+                self.__car_list.remove(car)
     
 car1=Car("WagonR",2010,"KA09 3056")
 car2=Car("Beat", 2011, "MH10 6776")
